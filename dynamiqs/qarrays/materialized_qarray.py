e332d751c9fd1cf524b7055cb50fa2fba2695486
Nicolas Lepage <nlepage@higgs.alice-bob.com>
2026-06-05 16:53:51 +0200

    added forgotten super to check_init

--- dynamiqs/qarrays/materialized_qarray.py
+++ dynamiqs/qarrays/materialized_qarray.py
@@ -29,7 +29,7 @@ class MaterializedQArray(QArray):
     def __check_init__(self):
 
         super().__check_init__()
-        
+
         # === ensure dims is compatible with the shape
         # for vectorized superoperators, we allow that the shape is the square
         # of the product of all dims
